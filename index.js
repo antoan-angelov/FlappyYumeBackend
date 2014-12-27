0341a467c42ccad21eb62d1e78824051815713ea
antoan-angelov <antoan.angelov@gmail.com>
2014-12-27 20:14:55 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var app = express();
+var mysql      = require('mysql');
+
+app.set('port', 3000);
+
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : '',
+  database: 'flappy_scores'
+});
+
+app.get(/^\/users\/(\w{3,})\/score\/(\d+)$/, function(request, response) {
+
+    var user  = { name: connection.escape(request.params[0]), score: connection.escape(request.params[1]) };
+    var query = connection.query('INSERT INTO scores SET ?', user, function(err, result) {
+      if (err) throw err;
+        
+        result.user = user;
+        response.send(result);
+    });
+});
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'));
+});
