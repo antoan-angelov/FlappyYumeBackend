e053cdc83dc051637235926970966733453a752f
antoan-angelov <antoan.angelov@gmail.com>
2014-12-28 01:33:57 +0200

    fix

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 var express = require('express');
 var app = express();
-//var mysql      = require('mysql');
+var mysql      = require('mysql');
 
 app.set('port', (process.env.PORT || 5000));
 
-/*var connection = mysql.createConnection({
+var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
   password : '',
   database: 'flappy_scores'
-});*/
+});
 
 app.get("/", function(request, response) {
 
@@ -17,14 +17,13 @@ app.get("/", function(request, response) {
 });
 
 app.get(/^\/users\/(\w{3,})\/score\/(\d+)$/, function(request, response) {
-response.send("score!");
-    /*var user  = { name: connection.escape(request.params[0]), score: connection.escape(request.params[1]) };
+    var user  = { name: connection.escape(request.params[0]), score: connection.escape(request.params[1]) };
     var query = connection.query('INSERT INTO scores SET ?', user, function(err, result) {
       if (err) throw err;
         
         result.user = user;
         response.send(result);
-    });*/
+    });
 });
 
 app.listen(app.get('port'), function() {
